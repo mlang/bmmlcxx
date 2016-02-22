287cafad3b05ad3bda88c564aab4e21f7ea30852
Mario Lang <mlang@delysid.org>
2016-02-22 23:26:55 +0100

    Experiment with parsing inaccord elements.

--- driver.cxx
+++ driver.cxx
@@ -9,51 +9,103 @@
 using namespace std;
 using namespace xml;
 
+using partial_voice = std::vector<std::shared_ptr<bmml::dom::element>>;
+using partial_measure = std::vector<partial_voice>;
+using voice = std::vector<partial_measure>;
+using measure = std::vector<voice>;
+
+bool is_layout_element(std::shared_ptr<bmml::dom::element> e) {
+  return dynamic_pointer_cast<bmml::space>(e)
+      || dynamic_pointer_cast<bmml::newline>(e)
+      || dynamic_pointer_cast<bmml::music_hyphen>(e)
+      || dynamic_pointer_cast<bmml::separator>(e)
+      || dynamic_pointer_cast<bmml::generic_text>(e)
+      || dynamic_pointer_cast<bmml::part_name>(e);
+}
+
+ostream& operator<<(ostream& out, measure const& m) {
+  for (auto v : m) {
+    out << "[";
+    for (auto pm : v) {
+      out << "{";
+      for (auto pv : pm) {
+        out << "(";
+        bool first = true;
+        for (auto e : pv) {
+          if (!first) cout << ", ";
+          out << e->tag_name();
+          first = false;
+        }
+        out << ")";
+      }
+      out << "}";
+    }
+    out << "]";
+  }
+
+  return out;
+}
+
 int main (int argc, char *argv[]) {
-  if (argc != 2) {
-    cerr << "usage: " << argv[0] << " <bmml-file>" << endl;
+  if (argc < 2) {
+    cerr << "usage: " << argv[0] << " [<bmml-file>...]" << endl;
     return EXIT_FAILURE;
   }
 
   try {
-    ifstream ifs { argv[1] };
+    for (int i = 1; i < argc; ++i) {
+      unsigned int full{0}, part{0}, division{0};
+      ifstream ifs{argv[i]};
 
-    if (ifs.good()) {
-      parser p{ifs, argv[1]};
+      if (ifs.good()) {
+        parser p{ifs, argv[i]};
 
-      p.next_expect(parser::start_element, "score", content::complex);
-      auto root = make_shared<bmml::score>(p, false);
-      p.next_expect(parser::end_element, "score");
+        p.next_expect(parser::start_element, "score", content::complex);
+        auto score = make_shared<bmml::score>(p, false);
+        p.next_expect(parser::end_element, "score");
 
-      cout << root;
+        for (auto sdc : *score->data()) {
+          if (auto ts = dynamic_pointer_cast<bmml::time_signature>(sdc)) {
+            cout << "global ts" << endl;
+          } else if (auto p = dynamic_pointer_cast<bmml::part>(sdc)) {
+            measure current_measure{};
+            current_measure.emplace_back();
+            current_measure.back().emplace_back();
+            current_measure.back().back().emplace_back();
+            for (auto pc : *p) {
+              if (auto inaccord = dynamic_pointer_cast<bmml::inaccord>(pc)) {
+                switch (inaccord->value()) {
+                case bmml::inaccord_t::full:
+                  current_measure.emplace_back();
+                case bmml::inaccord_t::part:
+                  current_measure.back().emplace_back();
+                case bmml::inaccord_t::division:
+                  current_measure.back().back().emplace_back();
+                }
+              } else if (auto bl = dynamic_pointer_cast<bmml::barline>(pc)) {
+                cout << current_measure << endl;
 
-      cout << endl << "List of notes and rests:" << endl;
-      for (auto x : *root->data()) {
-	if (auto p = dynamic_pointer_cast<bmml::part>(x)) {
-	  for (auto pe : *p) {
-	    if (auto n = dynamic_pointer_cast<bmml::note>(pe)) {
-	      cout << n << " =";
-	      auto nd = n->find_element<bmml::note_data>();
-	      if (auto dur = nd->find_element<bmml::duration>())
-		cout << ' ' << int(*dur);
-	      else if (auto app = nd->find_element<bmml::appoggiatura_ref>())
-		cout << ' ' << "appoggiatura";
-	      auto pch = nd->find_element<bmml::pitch>();
-	      cout << ' ' << int(*pch);
-	      if (auto al = nd->find_element<bmml::alteration>())
-		cout << ' ' << int(*al);
-	      cout << endl;
-	    } else if (auto r = dynamic_pointer_cast<bmml::rest>(pe)) {
-	      auto rd = r->find_element<bmml::rest_data>();
-	      auto dur = rd->find_element<bmml::duration>();
-	      cout << r << " = " << int(*dur) << endl;
-	    }
-	  }
-	}
+                current_measure.clear();
+                current_measure.emplace_back();
+                current_measure.back().emplace_back();
+                current_measure.back().back().emplace_back();
+              } else if (!is_layout_element(pc)) {
+                current_measure.back().back().back().push_back(pc);
+              }
+            }
+          }
+        }
+      } else {
+        cerr << "Unable to open '" << argv[i] << "'." << endl;
       }
 
-      serializer s(cout, "output");
-      root->serialize(s);
+      if (full && part && division) {
+        cout << argv[i] << ": "
+             << full << " full, "
+             << part << " part, "
+             << division << " division"
+             << endl;
+      }
     }
   } catch (const xml::exception& e) {
     cerr << e.what() << endl;
