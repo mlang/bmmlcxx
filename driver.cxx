e331f29cf3019309c013a05a6b4c3468892a23dc
Mario Lang <mlang@delysid.org>
2016-03-20 16:08:58 +0100

    Replace experimental code in driver.cxx with declarative grammar.

--- driver.cxx
+++ driver.cxx
@@ -3,15 +3,31 @@
 #include <xml/exception>
 
 #include "bmml.hxx"
+#include "spirit_x3.hxx"
+#include <boost/spirit/home/x3.hpp>
 
-using namespace std;
+using bmml::inaccord_t;
+using bmml::spirit::barline;
+using bmml::spirit::space;
+using bmml::spirit::newline;
+using bmml::spirit::separator;
+using bmml::spirit::inaccord;
+using bmml::spirit::generic_text;
+using bmml::spirit::music_hyphen;
+using bmml::spirit::part_name;
+using bmml::spirit::element;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::ifstream;
+namespace x3 = boost::spirit::x3;
 
-using partial_voice = bmml::dom::element::elements_type;
-using partial_measure = std::vector<partial_voice>;
-using voice = std::vector<partial_measure>;
-using measure = std::vector<voice>;
+using partial_voice_t = bmml::dom::element::elements_type;
+using partial_measure_t = std::vector<partial_voice_t>;
+using voice_t = std::vector<partial_measure_t>;
+using measure_t = std::vector<voice_t>;
 
-ostream& operator<<(ostream& out, measure const& m) {
+std::ostream& operator<<(std::ostream& out, measure_t const& m) {
   for (auto v : m) {
     out << "[";
     for (auto pm : v) {
@@ -42,52 +58,38 @@ ostream& operator<<(ostream& out, measure const& m) {
   return out;
 }
 
-bool is_layout_element(std::shared_ptr<bmml::dom::element> e) {
-  return e->is<bmml::space, bmml::newline,
-               bmml::music_hyphen, bmml::separator,
-               bmml::generic_text, bmml::part_name>();
-}
+auto layout = space() | newline()
+            | music_hyphen() | separator()
+            | generic_text() | part_name()
+            ;
+
+x3::rule<struct partial_voice_rule, partial_voice_t> const partial_voice = "partial_voice";
+x3::rule<struct partial_measure_rule, partial_measure_t> const partial_measure = "partial_measure";
+x3::rule<struct voice_rule, voice_t> const voice = "voice";
+x3::rule<struct measure_rule, measure_t> const measure = "measure";
+
+auto partial_voice_def = +(element() - (inaccord() | barline()));
+auto partial_measure_def = partial_voice % inaccord(inaccord_t::division);
+auto voice_def = partial_measure % inaccord(inaccord_t::part);
+auto measure_def = voice % inaccord(inaccord_t::full);
+
+BOOST_SPIRIT_DEFINE(partial_voice, partial_measure, voice, measure);
 
 // Put score content into part specific measure structures.
-std::map<std::string, std::vector<measure>>
-get_parts(shared_ptr<bmml::score> score) {
-  std::map<std::string, std::vector<measure>> parts;
+std::map<std::string, std::vector<measure_t>>
+get_parts(std::shared_ptr<bmml::score> score) {
+  std::map<std::string, std::vector<measure_t>> parts;
 
   for (auto sdc : *score->data()) {
     if (auto ts = sdc->as<bmml::time_signature>()) {
       cout << "global ts " << ts->values() << endl;
     } else if (auto p = sdc->as<bmml::part>()) {
-      measure current_measure{};
-      auto part_data = p->id()->as<bmml::part_data>();
-      if (part_data) cout << "Found part_data" << endl;
-
-      for (auto pc : *p) {
-        if (auto inaccord = pc->as<bmml::inaccord>()) {
-          switch (inaccord->value()) {
-          case bmml::inaccord_t::full:
-            current_measure.emplace_back();
-          case bmml::inaccord_t::part:
-            current_measure.back().emplace_back();
-          case bmml::inaccord_t::division:
-            current_measure.back().back().emplace_back();
-          }
-        } else if (auto bl = pc->as<bmml::barline>()) {
-          if (!current_measure.empty()) {
-            parts[p->id()].push_back(current_measure);
-
-            current_measure.clear();
-          }
-        } else if (!is_layout_element(pc)) {
-          if (current_measure.empty()) {
-            current_measure.emplace_back();
-            current_measure.back().emplace_back();
-            current_measure.back().back().emplace_back();
-          }
-
-          current_measure.back().back().back().emplace_back(move(pc));
-        }
+      std::vector<measure_t> measures;
+      auto iter = p->begin();
+      if (x3::phrase_parse(iter, p->end(),
+            measure % barline(), layout, measures)) {
+        parts[p->id()].insert(parts[p->id()].end(), measures.begin(), measures.end());
       }
-      if (!current_measure.empty()) parts[p->id()].push_back(current_measure);
     }
   }
 
--- spirit_x3.hxx
+++ spirit_x3.hxx
@@ -2,6 +2,7 @@
 #define BMML_SPIRIT_X3_HPP
 
 #include <boost/spirit/home/x3/core/parser.hpp>
+#include <boost/spirit/home/x3/core/skip_over.hpp>
 #include <boost/spirit/home/x3/support/traits/move_to.hpp>
 #include <bmml.hxx>
 
@@ -18,6 +19,8 @@ namespace bmml {
         bool parse( Iterator& first, Iterator const& last
                   , Context const& context, RContext& rcontext
                   , Attribute& attr) const {
+          boost::spirit::x3::skip_over(first, last, context);
+          if (first != last)
           if (auto val = std::dynamic_pointer_cast<T>(*first)) {
             boost::spirit::x3::traits::move_to(val, attr);
             ++first;
@@ -28,13 +31,67 @@ namespace bmml {
           return false;
         }
       };
+      template<typename T, typename Predicate>
+      struct dynamic_pointer_parser_with_predicate : boost::spirit::x3::parser<
+        dynamic_pointer_parser_with_predicate<T, Predicate>
+      > {
+        using attribute_type = std::shared_ptr<T>;
+        dynamic_pointer_parser_with_predicate(Predicate const& pred) : pred(pred) {}
+        template < typename Iterator, typename Context, typename RContext
+                 , typename Attribute>
+        bool parse( Iterator& first, Iterator const& last
+                  , Context const& context, RContext& rcontext
+                  , Attribute& attr) const {
+          boost::spirit::x3::skip_over(first, last, context);
+          if (first != last)
+          if (auto val = std::dynamic_pointer_cast<T>(*first)) {
+            auto iter = val->begin();
+            if (pred(*val)) {
+              boost::spirit::x3::traits::move_to(val, attr);
+              ++first;
 
-      template <typename T> dynamic_pointer_parser<T> shared_ptr = {};
-      inline auto with_id() { return shared_ptr<bmml::with_id>; }
-      inline auto generic_text() { return shared_ptr<bmml::generic_text>; }
-      inline auto newline() { return shared_ptr<bmml::newline>; }
-      inline auto score() { return shared_ptr<bmml::score>; }
-      inline auto space() { return shared_ptr<bmml::space>; }
+              return true;
+            }
+          }
+
+          return false;
+        }
+        Predicate pred;
+      };
+
+      template<typename T> dynamic_pointer_parser<T> shared_ptr() {
+        return {};
+      }
+      template<typename T, typename Predicate>
+      dynamic_pointer_parser_with_predicate<T, Predicate>
+      shared_ptr(Predicate&& pred) {
+        return {std::forward<Predicate>(pred)};
+      }
+      inline auto element() { return shared_ptr<bmml::dom::element>(); }
+      inline auto with_id() { return shared_ptr<bmml::with_id>(); }
+      inline auto with_id(std::string const& id) {
+        return shared_ptr<bmml::with_id>(
+          [id](bmml::with_id const& obj) {
+            return obj.id() == id;
+          }
+        );
+      }
+      inline auto barline() { return shared_ptr<bmml::barline>(); }
+      inline auto generic_text() { return shared_ptr<bmml::generic_text>(); }
+      inline auto inaccord() { return shared_ptr<bmml::inaccord>(); }
+      inline auto inaccord(bmml::inaccord_t value) {
+        return shared_ptr<bmml::inaccord>(
+          [value](bmml::inaccord const& obj) {
+            return obj.value() == value;
+          }
+        );
+      }
+      inline auto music_hyphen() { return shared_ptr<bmml::music_hyphen>(); }
+      inline auto newline() { return shared_ptr<bmml::newline>(); }
+      inline auto part_name() { return shared_ptr<bmml::part_name>(); }
+      inline auto score() { return shared_ptr<bmml::score>(); }
+      inline auto separator() { return shared_ptr<bmml::separator>(); }
+      inline auto space() { return shared_ptr<bmml::space>(); }
     }
   }
 }
